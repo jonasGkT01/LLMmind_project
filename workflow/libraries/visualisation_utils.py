3b04645df0bdd4aee0fc3b616763c7ece0060c70
Jonas Salvalaggio <jonas.salvalaggio@unito.it>
2026-09-18 15:33:43 +0200

    small cleanup

--- workflow/libraries/visualisation_utils.py
+++ workflow/libraries/visualisation_utils.py
@@ -22,7 +22,7 @@ def contrasting_text_color(image, value):
     r, g, b = rgba[:3]
 
     # Relative perceived luminance of the cell background.
-    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    luminance = 0.2126*r + 0.7152*g + 0.0722*b
 
     return "black" if luminance > 0.5 else "white"
 
